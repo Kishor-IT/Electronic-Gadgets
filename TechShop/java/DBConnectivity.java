bd8903ebab3f19b5333da7d119ed5742d6659b4b
Kishor-IT <125953988+Kishor-IT@users.noreply.github.com>
2025-04-11 13:16:05 +0530

    Update DBConnectivity.java

--- TechShop/java/DBConnectivity.java
+++ TechShop/java/DBConnectivity.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/TechShop"; // Replace with your DB name
-    private static final String USER = "root"; // Replace with your DB username
-    private static final String PASSWORD = "1234"; // Replace with your DB password
+    private static final String URL = "jdbc:mysql://localhost:3306/TechShop"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "1234"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
